5dac3168ef8267f58eb1b0ad00aa1e916e318562
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-08-24 17:04:14 -0400

    Use of function

---
+++ hello.js
@@ -0,0 +1,14 @@
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+
+//giving parameter
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Julia");
+sayHello("James");
